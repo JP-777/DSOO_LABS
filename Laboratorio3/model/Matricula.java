9f578b5c6c51eb1763febbe0753e15594bf64d53
elkinramos8 <eramosoch@unsa.edu.pe>
2025-09-24 20:18:48 -0500

    Definimos los atributos de la Clase matricula

--- Laboratorio3/model/Matricula.java
+++ Laboratorio3/model/Matricula.java
@@ -3,5 +3,10 @@ package model;
 import java.util.ArrayList;
 
 public class Matricula {
+    private String idMatricula;
+    private Alumno alumnoRef;
+    private Curso cursoRef;
+    private ArrayList<Double> notas;
+    private static final int LIMITE_NOTAS = 3;
 
 }
