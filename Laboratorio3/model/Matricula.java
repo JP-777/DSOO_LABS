df26542c0025fb2c8d2f21e6a22a28f3035b1bf1
elkinramos8 <eramosoch@unsa.edu.pe>
2025-09-24 20:19:38 -0500

    Definimos los metodos de la Clase matricula

--- Laboratorio3/model/Matricula.java
+++ Laboratorio3/model/Matricula.java
@@ -9,4 +9,43 @@ public class Matricula {
     private ArrayList<Double> notas;
     private static final int LIMITE_NOTAS = 3;
 
+    public Matricula (Alumno alumnoRef, Curso cursoRef, ArrayList<Double> notas){
+        this.alumnoRef = alumnoRef;
+        this.cursoRef = cursoRef;
+        this.notas = notas;
+    }
+
+    public String getIdMatricula() { return idMatricula; }
+    public Alumno getAlumnoRef() { return alumnoRef; }
+    public Curso getCursoRef() { return cursoRef; }
+    public ArrayList<Double> getNotas() { return notas; }
+
+    public void setIdMatricula(String idMatricula) { this.idMatricula = idMatricula; }
+    public void setAlumnoRef(Alumno alumnoRef) { this.alumnoRef = alumnoRef; }
+    public void setCursoRef(Curso cursoRef) { this.cursoRef = cursoRef; }
+
+    public void agregarNota (double nota){
+        if (nota < 0 || nota > 20 ) { 
+            System.out.println("La nota esta fuera de los limites");
+            return;
+        }
+        if (notas.size() < LIMITE_NOTAS){
+            notas.add(nota);
+        } else {
+            System.out.println("haz alcanzado el limite de " + LIMITE_NOTAS + " notas.");
+        }
+    }
+
+    public double calcularPromedio(){
+        if (notas.isEmpty()) return 0;
+        double sumatoria = 0;
+        for (int i=0; i<notas.size(); i++){
+            sumatoria += notas.get(i);
+        }
+        return Math.round((sumatoria/notas.size()) * 100.0)/ 100.0;
+    }
+
+    public boolean isAprobado(){
+        return calcularPromedio() >= 10.5;
+    }
 }
