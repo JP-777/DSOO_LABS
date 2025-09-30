f9d54eaf3bcad14e941cb52c92420e2f63240712
elkinramos8 <eramosoch@unsa.edu.pe>
2025-09-30 13:21:34 -0500

    Definiendo metodos solicitados de Usuario

--- Laboratorio4/model/Usuario.java
+++ Laboratorio4/model/Usuario.java
@@ -5,5 +5,25 @@ public class Usuario {
     private String nombre;
     private ArrayList<Libro> librosPrestados;
 
-    
+    public Usuario(String nombre){
+        this.nombre = nombre;
+        this.librosPrestados = new ArrayList<>();
+    }
+
+    public String getNombre(){ return nombre; }
+    public ArrayList<Libro> getLibrosPrestados() { return librosPrestados; }
+
+    public void tomarPrestado (Libro libro){
+        libro.setDisponible (false);
+        librosPrestados.add(libro);
+    }
+
+    public void devolverLibro(Libro libro){
+        libro.setDisponible(true);
+        librosPrestados.remove(libro);
+    }
+
+    public boolean verificarDisponible (Libro libro) {
+        return libro.isDisponible();
+    }
 }
