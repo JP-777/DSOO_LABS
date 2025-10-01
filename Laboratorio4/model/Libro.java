1088e1f4e8643d6eb9c250def00472d4251b55b0
Fernando-Solsol <fsolsol@unsa.edu.pe>
2025-10-01 12:47:15 -0500

    Subiendo primera parte del archivo Libro.java en la carpeta model.

--- Laboratorio4/model/Libro.java
+++ Laboratorio4/model/Libro.java
@@ -1,4 +1,23 @@
+package model;
+ public class Libro{
+    private String titulo;
+    private String autor;
+    private String ISBN;
+    private boolean disponible;
+    
+    public Libro(String titulo, String autor, String ISBN, boolean disponible){
+        this.titulo = titulo;
+        this.autor = autor;
+        this.ISBN = ISBN;
+        this.disponible = disponible;
+    }
 
+    public String getTitulo (){return titulo;}
+    public String getAutor (){return autor;}
+    public String getISBN (){return ISBN;}
+    public boolean estaDisponible (){return disponible;}
+    
+ }
 // Para la clase Usuario se hizo uso de la disponibilidad del libro
     //Para el metodo accesor usar isDisponible()
     //Para el metodo mutador usar getDisponible()
