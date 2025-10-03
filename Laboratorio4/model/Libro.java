7c4cb2bce8ebc64368a2a62ab1d72fafdefff5a8
Fernando-Solsol <fsolsol@unsa.edu.pe>
2025-10-02 21:28:41 -0500

    Subiendo actualización del archivo Libro.java

--- Laboratorio4/model/Libro.java
+++ Laboratorio4/model/Libro.java
@@ -1,6 +1,6 @@
 package model;
 public class Libro{
-    private String titulo;
+    private String titulo; 
     private String autor;
     private String ISBN;
     private boolean disponible; 
@@ -11,23 +11,25 @@ public class Libro{
         this.ISBN = ISBN;
         this.disponible = disponible;
     }
-    
     //constructor vacío para crear objeto sin inicializarlo todavía
     //Se debe llenar en el orden preestablecido de los atributos
     public Libro(){
         this("", "", "", true);
     }
-
+    //Constructor para cuando el libro es de autor anónimo
+    public Libro(String titulo, String ISBN, boolean disponible){
+        this.titulo = titulo;
+        this.ISBN = ISBN;
+        this.disponible = disponible;
+    }
     public String getTitulo (){return titulo;}
     public String getAutor (){return autor;}
     public String getISBN (){return ISBN;}
     public boolean estaDisponible (){return disponible;}
-    
     public void setTitulo(String titulo){this.titulo = titulo;}
     public void setAutor(String autor){this.autor = autor;}
     public void setISBN(String ISBN){this.ISBN = ISBN;}
     public void setDisponible(boolean disponible){this.disponible = disponible;}
-
     @Override
     public String toString(){
         return "Título: "+titulo+", Autor: "+autor+", ISBN: "+ISBN+", Disponibilidad: "+(disponible);
