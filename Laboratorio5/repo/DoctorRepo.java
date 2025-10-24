9681df419b594a963617c55cfaa43ef350046f14
Fernando-Solsol <fsolsol@unsa.edu.pe>
2025-10-24 16:56:32 -0500

    Correccion de DoctorRepo.java

--- Laboratorio5/repo/DoctorRepo.java
+++ Laboratorio5/repo/DoctorRepo.java
@@ -1,8 +1,6 @@
 package repo;
-
 import java.util.ArrayList;
 import model.Doctor;
-
 public class DoctorRepo {
     private ArrayList<Doctor> listaDoctores;
     public DoctorRepo() {
@@ -17,4 +15,23 @@ public class DoctorRepo {
         System.out.println("Doctor agregado correctamente.");
         return true;
     }
+    public Doctor buscarPorCodigo(int codigo) {
+        for (Doctor doc : listaDoctores) {
+            if (doc.getCodigo() == codigo) {
+                return doc;
+            }
+        }
+        return null;
+    }
+    public void mostrarDoctorPorCodigo(int codigo) {
+        Doctor doc = buscarPorCodigo(codigo);
+        if (doc != null) {
+            System.out.println("Doctor encontrado: " + doc.getNombre() +
+                ", Especialidad: " + doc.getEspecialidad() +
+                ", Horario: " + doc.getHorarioAtencion());
+        } else {
+            System.out.println("No se encontró un doctor con el código " + codigo);
+        }
+    }
 }
+
