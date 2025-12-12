b2257d2187cca83caefe64cf207b1b65736903b2
elkinramos8 <eramosoch@unsa.edu.pe>
2025-12-12 16:12:02 -0500

    eliminacion de clases de almacenamiento en memoria

--- laboratorio9/repo/DepositoRepo.java
+++
@@ -1,9 +0,0 @@
-package DSOO_LABS.laboratorio7.repo;
-
-import DSOO_LABS.laboratorio7.model.Deposito;
-
-public class DepositoRepo extends TransaccionRepo {
-    public void registrarDeposito(Deposito d) {
-        super.agregarTransaccion(d);
-    }
-}
--- laboratorio9/repo/RetiroRepo.java
+++
@@ -1,9 +0,0 @@
-package DSOO_LABS.laboratorio7.repo;
-
-import DSOO_LABS.laboratorio7.model.Retiro;
-
-public class RetiroRepo extends TransaccionRepo {
-    public void registrarRetiro(Retiro r) {
-        super.agregarTransaccion(r);
-    }
-}
